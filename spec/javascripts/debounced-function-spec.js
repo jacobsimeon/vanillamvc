995c3a13b970606c55acdce95ec2beda977d350b
Jacob Morris <jacob.s.morris@gmail.com>
2016-03-22 12:16:07 -0600

    add tests for debounced function

--- spec/javascripts/debounced-function-spec.js
+++ spec/javascripts/debounced-function-spec.js
@@ -1,5 +1,43 @@
 describe("DebouncedFunction", function() {
-  it("exists", function() {
-    expect(DebouncedFunction).not.toBeUndefined();
+  describe("#call", function() {
+    it("invokes the wrapped function a single time within 400ms ", function(done) {
+      var spy = jasmine.createSpy("bounce me");
+      var debouncedSpy = debounce(spy);
+
+      debouncedSpy.call();
+      debouncedSpy.call();
+      debouncedSpy.call();
+      debouncedSpy.call();
+
+      expect(spy).not.toHaveBeenCalled();
+      setTimeout(function() {
+        expect(spy.calls.count()).toEqual(1);
+        done();
+      }, 500);
+    });
+  });
+
+  describe("#callImmediately", function() {
+    it("calls the wrapped function immediately", function() {
+      var spy = jasmine.createSpy("bounce me");
+      var debouncedSpy = debounce(spy);
+
+      debouncedSpy.callImmediately();
+
+      expect(spy).toHaveBeenCalled();
+    });
+
+    it("clears any previously queued calls", function(done) {
+      var spy = jasmine.createSpy("bounce me");
+      var debouncedSpy = debounce(spy);
+
+      debouncedSpy.call();
+      debouncedSpy.callImmediately();
+
+      setTimeout(function() {
+        expect(spy.calls.count()).toEqual(1);
+        done();
+      }, 500);
+    });
   });
 });
